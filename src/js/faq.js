d4660e8cd4d6e40cfbf162be11568587b6a8f362
Maryna Boholiubova <astoret2015@gmail.com>
2026-04-06 18:49:47 +0200

    new-faq

---
+++ src/js/faq.js
@@ -0,0 +1,17 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+export function initFaqAccordion() {
+  const container = document.querySelector('.accordion-container');
+
+  if (container) {
+    new Accordion(container, {
+      duration: 400,
+      showMultiple: false,
+      collapse: true,
+      elementClass: 'ac',
+      triggerClass: 'ac-trigger',
+      panelClass: 'ac-panel',
+    });
+  }
+}
