20a98e9480a0695c8d49d46b71e8d78b1447b57a
Volodymyr Lantrat <stivenuaking@gmail.com>
2026-04-06 09:42:51 +0300

    add js(render filter list & card gallery) to furniture-list

--- src/js/api.js
+++ src/js/api.js
@@ -9,12 +9,26 @@ const api = axios.create({
   },
 });
 
-export async function getFurnitures(page = 1, limit = 10) {
-  const response = await api.get(`/furnitures?page=${page}&limit=${limit}`);
-  return response.data;
+export async function getFurnitures(page = 1, limit = 30, category) {
+  try {
+    let url = `/furnitures?page=${page}&limit=${limit}`;
+    if (category && category !== "all") {
+      url += `&category=${encodeURIComponent(category)}`;
+    }
+    const response = await api.get(url);
+    return response.data;
+  } catch (err) {
+    console.error("Ошибка запроса мебели:", err);
+    throw err;
+  }
 }
 
 export async function getCategories() {
-  const response = await api.get("/categories");
-  return response.data;
+  try {
+    const response = await api.get("/categories");
+    return response.data;
+  } catch (err) {
+    console.error("Ошибка запроса категорий:", err);
+    throw err;
+  }
 }
